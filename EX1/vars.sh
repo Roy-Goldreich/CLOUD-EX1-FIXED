a251db445fe79d80a9f4d35409b3aac5193337f8
Roy-Goldreich <74922421+Roy-Goldreich@users.noreply.github.com>
2021-05-11 19:52:01 +0300

    Submission of AWS parking lot manager

---
+++ EX1/vars.sh
@@ -0,0 +1,8 @@
+HOST_DOM="mysql-instance.cstlcqm0bmbz.us-east-2.rds.amazonaws.com"
+DB_PORT=3306
+DB_PASSWORD=secret999999
+DB_USERNAME=admin
+HOST_DOM="mysql-instance.cstlcqm0bmbz.us-east-2.rds.amazonaws.com"
+DB_PORT=3306
+DB_PASSWORD=secret999999
+DB_USERNAME=admin
